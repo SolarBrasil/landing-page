fb6cbe966dbe0659eed4d03406d26288b7ab4f97
Patrick Martins <cledcppm@gmail.com>
2024-12-23 16:45:29 -0300

    Create sitemap.js

---
+++ app/sitemap.js
@@ -0,0 +1,40 @@
+export default function sitemap() {
+    return [
+        {
+            url: 'https://solarb.com.br',
+            lastModified: new Date(),
+            changeFrequency: 'yearly',
+            priority: 1,
+        },
+        {
+            url: 'https://www.solarb.com.br',
+            lastModified: new Date(),
+            changeFrequency: 'yearly',
+            priority: 1,
+        },
+        {
+            url: 'https://solarb.com.br/contatos/maraba',
+            lastModified: new Date(),
+            changeFrequency: 'yearly',
+            priority: 0.8,
+        },
+        {
+            url: 'https://solarb.com.br/contatos/sao-luis',
+            lastModified: new Date(),
+            changeFrequency: 'yearly',
+            priority: 0.8,
+        },
+        {
+            url: 'https://www.solarb.com.br/contatos/maraba',
+            lastModified: new Date(),
+            changeFrequency: 'yearly',
+            priority: 0.8,
+        },
+        {
+            url: 'https://www.solarb.com.br/contatos/sao-luis',
+            lastModified: new Date(),
+            changeFrequency: 'yearly',
+            priority: 0.8,
+        },
+    ]
+}
