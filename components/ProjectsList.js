3a2f629a0cee0c43534e6f5d1182570044aff594
Patrick Martins <cledcppm@gmail.com>
2024-12-24 11:04:42 -0300

    Images optimizations
    Make images smaller and adds blurred image placeholder.

---
+++ components/ProjectsList.js
@@ -0,0 +1,101 @@
+import {BuildingStorefrontIcon, HomeModernIcon, ShoppingCartIcon} from "@heroicons/react/24/outline";
+
+import ArtsPreMoldados from "/public/images/project_artes.png"
+import Concreta from "/public/images/project_concreta.png"
+import DiaaDia from "/public/images/project_diaadia.png"
+import DomConcept from "/public/images/project_domconcept.png"
+import Eugenio from "/public/images/project_eugenio.png"
+import FKCeramica from "/public/images/project_fkceramica.png"
+import HidraulicaFederal from "/public/images/project_hidraulicafederal.png"
+import Natuclara from "/public/images/project_natuclara.png"
+import NorteSul from "/public/images/project_nortesul.png"
+import PostoWR from "/public/images/project_postowr.png"
+import QueijoLeite from "/public/images/project_queijoleite.png"
+import WRSupermercado from "/public/images/project_wrsupermercado.png"
+
+export const Projects = [
+    {
+        "Icon": HomeModernIcon,
+        "src": ArtsPreMoldados,
+        "name": "Artes em Pré-Moldados",
+        "power": "10.000 kWh/Mês",
+        "caption": "Fabricação de estruturas pré-moldadas de concreto armado em Marabá, PA."
+    },
+    {
+        "Icon": HomeModernIcon,
+        "src": Concreta,
+        "name": "Concreta Engenharia",
+        "power": "6.000 kWh/Mês",
+        "caption": "Construçao de edifícos em Marabá, PA."
+    },
+    {
+        "Icon": ShoppingCartIcon,
+        "src": DiaaDia,
+        "name": "Supermercado Dia a Dia",
+        "power": "5.000 kWh/Mês",
+        "caption": "Supermercado em Marabá, MA."
+    },
+    {
+        "Icon": BuildingStorefrontIcon,
+        "src": DomConcept,
+        "name": "Dom Concept",
+        "power": "10.500 kWh/Mês",
+        "caption": "Cabelereiros em São Luís, MA."
+    },
+    {
+        "Icon": HomeModernIcon,
+        "src": FKCeramica,
+        "name": "FK Cerâmica",
+        "power": "12.000 kWh/Mês",
+        "caption": "Indústria cerâmica em Nova Ipixuna, PA."
+    },
+    {
+        "Icon": HomeModernIcon,
+        "src": HidraulicaFederal,
+        "name": "Hidraúlica Federal",
+        "power": "5.700 kWh/Mês",
+        "caption": "Manutenção e reparação de equipamentos hidráulicos e pneumáticos em Marabá, PA."
+    },
+    {
+        "Icon": HomeModernIcon,
+        "src": Natuclara,
+        "name": "Água Natuclara",
+        "power": "13.000 kWh/Mês",
+        "caption": "Fabricação de águas envasadas em Curionópolis, PA."
+    },
+    {
+        "Icon": HomeModernIcon,
+        "src": NorteSul,
+        "name": "Norte Sul Serviços",
+        "power": "14.000 kWh/Mês",
+        "caption": "Fabricação de estruturas metálicas em Parauapebas, PA."
+    },
+    {
+        "Icon": BuildingStorefrontIcon,
+        "src": PostoWR,
+        "name": "Autoposto WR",
+        "power": "8.000 kWh/Mês",
+        "caption": "Posto de combustíveis em São Pedro da Água Branca, MA."
+    },
+    {
+        "Icon": HomeModernIcon,
+        "src": QueijoLeite,
+        "name": "Laticínios Queijoleite",
+        "power": "13.000 kWh/Mês",
+        "caption": "Fabricação de laticínios em Água Azul no Norte, PA."
+    },
+    {
+        "Icon": ShoppingCartIcon,
+        "src": WRSupermercado,
+        "name": "W. R. Supermercado",
+        "power": "6.000 kWh/Mês",
+        "caption": "Supermercado em São Pedro da Água Branca, MA."
+    },
+    {
+        "Icon": HomeModernIcon,
+        "src": Eugenio,
+        "name": "Cerâmica Pai e Filho",
+        "power": "9.000 kWh/Mês",
+        "caption": "Indústria cerâmica em Nova Ipixuna, PA."
+    },
+];
--- components/SuccessfulProjects.js
+++ components/SuccessfulProjects.js
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import {BuildingStorefrontIcon, ShoppingCartIcon, HomeModernIcon} from "@heroicons/react/24/outline";
+import {Projects} from "@components/ProjectsList"
 
 function Project({Icon, src, name, power, caption}) {
     return (
@@ -12,6 +12,7 @@ function Project({Icon, src, name, power, caption}) {
                     className="w-full"
                     src={src}
                     alt={caption}
+                    placeholder="blur"
                 />
 
                 <blockquote className="w-full absolute top-0 left-0 text-sm">
@@ -35,93 +36,6 @@ function Project({Icon, src, name, power, caption}) {
 }
 
 export default function SuccessfulProjects() {
-    const projects = [
-        {
-            "Icon": HomeModernIcon,
-            "src": "/images/project_artes.png",
-            "name": "Artes em Pré-Moldados",
-            "power": "10.000 kWh/Mês",
-            "caption": "Fabricação de estruturas pré-moldadas de concreto armado em Marabá, PA."
-        },
-        {
-            "Icon": HomeModernIcon,
-            "src": "/images/project_concreta.png",
-            "name": "Concreta Engenharia",
-            "power": "6.000 kWh/Mês",
-            "caption": "Construçao de edifícos em Marabá, PA."
-        },
-        {
-            "Icon": ShoppingCartIcon,
-            "src": "/images/project_diaadia.png",
-            "name": "Supermercado Dia a Dia",
-            "power": "5.000 kWh/Mês",
-            "caption": "Supermercado em Marabá, MA."
-        },
-        {
-            "Icon": BuildingStorefrontIcon,
-            "src": "/images/project_domconcept.png",
-            "name": "Dom Concept",
-            "power": "10.500 kWh/Mês",
-            "caption": "Cabelereiros em São Luís, MA."
-        },
-        {
-            "Icon": HomeModernIcon,
-            "src": "/images/project_fkceramica.png",
-            "name": "FK Cerâmica",
-            "power": "12.000 kWh/Mês",
-            "caption": "Indústria cerâmica em Nova Ipixuna, PA."
-        },
-        {
-            "Icon": HomeModernIcon,
-            "src": "/images/project_hidraulicafederal.png",
-            "name": "Hidraúlica Federal",
-            "power": "5.700 kWh/Mês",
-            "caption": "Manutenção e reparação de equipamentos hidráulicos e pneumáticos em Marabá, PA."
-        },
-        {
-            "Icon": HomeModernIcon,
-            "src": "/images/project_natuclara.png",
-            "name": "Água Natuclara",
-            "power": "13.000 kWh/Mês",
-            "caption": "Fabricação de águas envasadas em Curionópolis, PA."
-        },
-        {
-            "Icon": HomeModernIcon,
-            "src": "/images/project_nortesul.png",
-            "name": "Norte Sul Serviços",
-            "power": "14.000 kWh/Mês",
-            "caption": "Fabricação de estruturas metálicas em Parauapebas, PA."
-        },
-        {
-            "Icon": BuildingStorefrontIcon,
-            "src": "/images/project_postowr.png",
-            "name": "Autoposto WR",
-            "power": "8.000 kWh/Mês",
-            "caption": "Posto de combustíveis em São Pedro da Água Branca, MA."
-        },
-        {
-            "Icon": HomeModernIcon,
-            "src": "/images/project_queijoleite.png",
-            "name": "Laticínios Queijoleite",
-            "power": "13.000 kWh/Mês",
-            "caption": "Fabricação de laticínios em Água Azul no Norte, PA."
-        },
-        {
-            "Icon": ShoppingCartIcon,
-            "src": "/images/project_wrsupermercado.png",
-            "name": "W. R. Supermercado",
-            "power": "6.000 kWh/Mês",
-            "caption": "Supermercado em São Pedro da Água Branca, MA."
-        },
-        {
-            "Icon": HomeModernIcon,
-            "src": "/images/project_eugenio.png",
-            "name": "Cerâmica Pai e Filho",
-            "power": "9.000 kWh/Mês",
-            "caption": "Indústria cerâmica em Nova Ipixuna, PA."
-        },
-    ];
-
     return (
         <section className="flex flex-col gap-8 px-4 lg:px-0">
             <header>
@@ -134,7 +48,7 @@ export default function SuccessfulProjects() {
 
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-y-10 gap-x-5 justify-center">
                 {
-                    projects.map((project, index) =>
+                    Projects.map((project, index) =>
                         <Project key={index} {...project}/>
                     )
                 }
